490b35f13a476a3238dbec76ff2176830e7bb2bb
Reda Doukali farji <rdoukali@3-C-8.42heilbronn.de>
2024-04-19 00:08:53 +0200

    rdoukali

---
+++ game_client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <GameBlock />
+  </div>
+</template>
+
+<script>
+import GameBlock from './components/GameBlock.vue'
+
+export default {
+  name: 'app',
+  components: {
+    GameBlock,
+  }
+}
+</script>
+
+<style></style>
